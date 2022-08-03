8373e8edf83be1ddbf99fa08ea3fa9d287693378
Darrell <DT@Terastar.biz>
2022-08-03 18:15:35 -0400

    Save ordering/filters (#570)

--- ui/src/stores.js
+++ ui/src/stores.js
@@ -1,11 +1,18 @@
 import { readable } from 'svelte/store';
 
 export const devices = readable([], function start(set) {
+	var errors = 0;
 	const interval = setInterval(() => {
 		fetch(`/json/devices`)
 			.then(d => d.json())
-			.then(r => set(r.devices))
-			.catch((ex) => { set(null); console.log(ex); });
+			.then(r => {
+				errors = 0;
+				set(r);
+			})
+			.catch((ex) => {
+				if (errors > 5) set(null);
+				console.log(ex);
+			});
 	}, 1000)
 
 	return function stop() {
