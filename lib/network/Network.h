e87d10248899a65f2698a76795dd5715da9af610
mjtimmer <104372728+mjtimmer@users.noreply.github.com>
2022-11-29 14:13:20 +0100

    Update Network.h
    Add ethernet support for GL-inet GL-S10.

--- lib/network/Network.h
+++ lib/network/Network.h
@@ -138,6 +138,16 @@ const ethernet_settings ethernetBoards[] = {
     18,                   // eth_mdio,
     ETH_PHY_LAN8720,      // eth_type,
     ETH_CLOCK_GPIO17_OUT  // eth_clk_mode
+  },
+  
+  // GL-inet GL-S10 v2.1 Ethernet
+  {
+    1,                    // eth_address,
+    5,                    // eth_power,
+    23,                   // eth_mdc,
+    18,                   // eth_mdio,
+    ETH_PHY_IP101,        // eth_type,
+    ETH_CLOCK_GPIO17_OUT  // eth_clk_mode
   }
 };
 
