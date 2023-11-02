2e0797e4769ab2c2eb5f7c7374ff6e2a87267de5
DTTerastar <DT@Terastar.biz>
2023-11-02 18:16:45 -0400

    Improve the prompt for pairing

--- ui/src/routes/Devices.svelte
+++ ui/src/routes/Devices.svelte
@@ -1,28 +1,30 @@
 <script>
-  import { devices, events, enroll } from "../stores";
-  let name;
+    import { devices, events, enroll } from "../stores";
+    let name;
 
-  function slugify(name) {
-    return name
-      .toString()
-      .normalize("NFD") // split an accented letter in the base letter and the acent
-      .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
-      .toLowerCase()
-      .trim()
-      .replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
-      .replace(/\s+/g, "-"); // separator
-  }
+    function slugify(name) {
+        return name
+            .toString()
+            .normalize("NFD") // split an accented letter in the base letter and the acent
+            .replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+            .toLowerCase()
+            .trim()
+            .replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+            .replace(/\s+/g, "-"); // separator
+    }
 </script>
 
 {#if $events?.state != null}
-  <label for="name">Name</label>
-  <input id="name" type="text" class="bg-gray-200 relative" placeholder="Name" bind:value={name} />
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed" disabled={$events.state.enrolling} on:click={() => enroll(name)}> Enroll </button>
+    <label for="name">Name</label>
+    <input id="name" type="text" class="bg-gray-200 relative" placeholder="Name" bind:value={name} />
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-50 disabled:cursor-not-allowed" disabled={$events.state.enrolling} on:click={() => enroll(name)}> Enroll </button>
 
-  {#if $events?.state?.enrolling}
-    <p>
-      You can now use the Bluetooth settings on your device and pair it with the ESPresense device. Once paired your device will be shown with the name: <span class="bg-gray-200 relative whitespace-nowrap">{slugify(name)}</span>
-      in the fingerprint page as well as on mqtt.
-    </p>
-  {/if}
+    {#if $events?.state?.enrolling}
+        <p>
+            To begin, navigate to your device's Bluetooth settings and pair with the ESPresense device. After obtaining the IRK, ESPresense will disconnect; there's no need to maintain the pairing on your device. Your device will display the name: <span class="bg-gray-400 accent-inherit relative whitespace-nowrap">{slugify(name)}</span> on both the fingerprint page and MQTT. This configuration is saved as a retained message in MQTT and will be loaded by all ESPresense devices on the same MQTT server.
+        </p>
+        <p>
+            Start the pairing process now.
+        </p>
+    {/if}
 {/if}
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     proxy: {
       '^/(json|extras|restart)': {
-        target: 'http://192.168.128.134/',
+        target: 'http://192.168.128.180/',
         changeOrigin: true,
         rewrite: (path) => path
       },
       '^/ws': {
-        target: 'ws://192.168.128.134/',
+        target: 'ws://192.168.128.180/',
         changeOrigin: true,
         rewrite: (path) => path
       },
