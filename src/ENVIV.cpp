0d3de4c09313a5d6b58050911bee355784bc9375
dxmnkd316 <46753733+dxmnkd316@users.noreply.github.com>
2024-09-30 11:12:57 -0500

    Update ENVIV.cpp - Revise variables to match UI names

--- src/ENVIV.cpp
+++ src/ENVIV.cpp
@@ -13,8 +13,8 @@ namespace ENVIV
     BMP280 bmp;
     SHT4X sht;
     //long BMP280_status;
-    String BMP280_I2c;
-    int BMP280_I2c_Bus;  
+    String ENVIVBMP280_I2c;
+    int ENVIVBMP280_I2c_Bus;  
     unsigned long BMP280PreviousMillis = 0;
     int sensorInterval = 60000;
     bool initialized = false;
@@ -53,8 +53,8 @@ namespace ENVIV
     void ConnectToWifi()
     {
         //Serial.println("env IV connect to wifi");
-        BMP280_I2c_Bus = AsyncWiFiSettings.integer("BMP280_I2c_Bus", 1, 2, DEFAULT_I2C_BUS, "I2C Bus");
-        BMP280_I2c = AsyncWiFiSettings.string("BMP280_I2c", "", "I2C address (0x76 or 0x77)");
+        ENVIVBMP280_I2c_Bus = AsyncWiFiSettings.integer("ENVIVBMP280_I2c_Bus", 1, 2, DEFAULT_I2C_BUS, "I2C Bus");
+        ENVIVBMP280_I2c = AsyncWiFiSettings.string("ENVIVBMP280_I2c", "", "I2C address (0x76 or 0x77)");
     }
 
     void SerialReport()
@@ -62,13 +62,14 @@ namespace ENVIV
         if (!I2C_Bus_1_Started && !I2C_Bus_2_Started) return;
         //if (BMP280m5_I2c.isEmpty()) return;
         Serial.print("ENVIV BMP280:       ");
-        Serial.println(BMP280_I2c + " on bus " + BMP280_I2c_Bus);
+        Serial.println(ENVIVBMP280_I2c + " on bus " + ENVIVBMP280_I2c_Bus);
     }
 
     void Loop()
     {
         if (!I2C_Bus_1_Started && !I2C_Bus_2_Started) return;
         if (!initialized) return;
+        if (!initializedsht) return;
 
         if (BMP280PreviousMillis == 0 || millis() - BMP280PreviousMillis >= sensorInterval) {
 
@@ -90,7 +91,7 @@ namespace ENVIV
     bool SendDiscovery()
     {
         //Serial.println("env IV send discovery");
-        //if (BMP280_I2c.isEmpty()) return true;
+        if (ENVIVBMP280_I2c.isEmpty()) return true;
         return sendSensorDiscovery("ENVIV Temperature", EC_NONE, "temperature", "°C")
             && sendSensorDiscovery("ENVIV Pressure", EC_NONE, "pressure", "hPa")
             && sendSensorDiscovery("SHT40 Temperature", EC_NONE, "temperature", "°C")
