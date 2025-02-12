243685251a5d0531d8697e4dca527f6021600051
DTTerastar <DT@Terastar.biz>
2025-02-12 05:16:28 -0500

    Fix save button

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -22,11 +22,11 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '/json': 'http://192.168.129.176',
-            '/wifi': 'http://192.168.129.176',
-            '/restart': 'http://192.168.129.176',
+            '/json': 'http://192.168.129.178',
+            '/wifi': 'http://192.168.129.178',
+            '/restart': 'http://192.168.129.178',
             '/ws': {
-                target: 'ws://192.168.129.176',
+                target: 'ws://192.168.129.178',
                 ws: true,
             }
         }
