83a16ffdeae8cf9393795a0f6c1357cbc50d6de8
Darrell <DT@Terastar.biz>
2025-06-28 10:12:58 -0400

    wrap global styles in layer

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -1,10 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{html,js,svelte,ts}',
-    './.svelte-kit/**/*.{html,js,svelte,ts}',
-    './node_modules/svelte-table/**/*.{html,js,svelte,ts}'
-  ],
   theme: {
     extend: {},
   },
