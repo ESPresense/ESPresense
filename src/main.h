78669f43f84a12ba7be7adb8c0597f3a41b7f866
DTTerastar <DT@Terastar.biz>
2026-02-10 08:04:08 -0500

    Add ESP32-C6 support and refactor network handling

--- src/main.h
+++ src/main.h
@@ -21,7 +21,7 @@
 #include "Motion.h"
 #include "Switch.h"
 #include "Button.h"
-#include "Network.h"
+#include "EspresenseNet.h"
 #include "SerialImprov.h"
 #include "Logger.h"
 #include "Updater.h"
