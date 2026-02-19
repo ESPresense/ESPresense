30e40452d14010baf964a6616641fabe8bc0f448
Sensie <sensie@espresense.com>
2026-02-19 10:28:45 -0500

    feat(led): add configurable PWM fade time

--- src/led/SinglePWM.cpp
+++ src/led/SinglePWM.cpp
@@ -1,8 +1,9 @@
 #include "SinglePWM.h"
 
-SinglePWM::SinglePWM(uint8_t index, ControlType controlType, bool inverted, int pin) : LED(index, controlType) {
+SinglePWM::SinglePWM(uint8_t index, ControlType controlType, bool inverted, int pin, uint16_t fadeTimeMs) : LED(index, controlType) {
     this->inverted = inverted;
     this->pin = pin;
+    this->fadeTimeMs = fadeTimeMs;
 }
 
 void SinglePWM::init() {
@@ -17,14 +18,41 @@ void SinglePWM::init() {
 }
 
 void SinglePWM::update() {
-    setDuty(LED::getState() ? LED::getBrightness() : 0);
+    uint32_t const newTargetDuty = toDuty(LED::getState() ? LED::getBrightness() : 0);
+
+    if (!inited) {
+        init();
+        writeDuty(newTargetDuty);
+        startDuty = targetDuty = newTargetDuty;
+        fadeActive = false;
+        return;
+    }
+
+    if (newTargetDuty == targetDuty && !fadeActive) {
+        return;
+    }
+
+    if (fadeTimeMs == 0) {
+        writeDuty(newTargetDuty);
+        startDuty = targetDuty = newTargetDuty;
+        fadeActive = false;
+        return;
+    }
+
+    startDuty = currentDuty;
+    targetDuty = newTargetDuty;
+    fadeStartMs = millis();
+    fadeActive = true;
 }
 
-void SinglePWM::setDuty(uint32_t x) {
-    if (!inited) init();
-    if (!inited) return;
+uint32_t SinglePWM::toDuty(uint32_t x) const {
     uint32_t duty = x >= 255 ? 4096 : (x <= 0 ? 0 : round(4096.0 * pow(10.0, 0.0055 * (x - 255.0))));
     if (inverted) duty = 4096 - duty;
+    return duty;
+}
+
+void SinglePWM::writeDuty(uint32_t duty) {
+    currentDuty = duty;
 #ifdef ARDUINO_V3
     ledcWrite(pin, duty);
 #else
@@ -33,4 +61,16 @@ void SinglePWM::setDuty(uint32_t x) {
 }
 
 void SinglePWM::service() {
+    if (!fadeActive) return;
+
+    uint32_t const elapsed = millis() - fadeStartMs;
+    if (elapsed >= fadeTimeMs) {
+        writeDuty(targetDuty);
+        fadeActive = false;
+        return;
+    }
+
+    int32_t const delta = (int32_t)targetDuty - (int32_t)startDuty;
+    int32_t const nextDuty = (int32_t)startDuty + (delta * (int32_t)elapsed) / (int32_t)fadeTimeMs;
+    writeDuty((uint32_t)constrain(nextDuty, 0, 4096));
 }
