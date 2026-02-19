5c1cd797417d63166dbb7bc23bb60129867061ea
Sensie <sensie@espresense.com>
2026-02-19 08:34:13 -0500

    fix: update renamed ESPresenseNetwork header includes

--- src/main.h
+++ src/main.h
@@ -21,7 +21,7 @@
 #include "Motion.h"
 #include "Switch.h"
 #include "Button.h"
-#include "../lib/ESPresenseNetwork/Network.h"
+#include "../lib/ESPresenseNetwork/ESPresenseNetwork.h"
 #include "SerialImprov.h"
 #include "Logger.h"
 #include "Updater.h"
