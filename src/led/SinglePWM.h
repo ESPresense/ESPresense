e51f17bf5bc86ec2a00099b69cc7b5b6ad5d21ca
DTTerastar <DT@Terastar.biz>
2026-03-29 16:16:11 -0400

    Revert "feat: configurable PWM LED fade time (fixes #1138) (#2261)"
    This reverts commit ab27e8b348a21820ac766ca11140458a55acdabc.

--- src/led/SinglePWM.h
+++ src/led/SinglePWM.h
@@ -5,21 +5,14 @@
 class SinglePWM : public LED {
    public:
 
-    SinglePWM(uint8_t index, ControlType controlType, bool inverted, int pin, uint16_t fadeTimeMs = 0);
+    SinglePWM(uint8_t index, ControlType controlType, bool inverted, int pin);
     void update() override;
     void service() override;
 
    private:
     void init();
-    uint32_t toDuty(uint32_t value) const;
-    void writeDuty(uint32_t duty);
+    void setDuty(uint32_t value);
     bool inverted;
     int pin;
-    uint16_t fadeTimeMs;
-    uint32_t currentDuty = 0;
-    uint32_t startDuty = 0;
-    uint32_t targetDuty = 0;
-    uint32_t fadeStartMs = 0;
-    bool fadeActive = false;
     bool inited = false;
 };
